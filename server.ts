92de87ddd72281038925cfd16c4858eb57d8e0d0
Suryansh Anand <spirit@anandsur.com>
2024-10-24 20:03:36 +0530

    feat(b): setup orm

--- server.ts
+++ server.ts
@@ -5,9 +5,10 @@ import express from "express";
 import waitOn from "wait-on";
 import onExit from "signal-exit";
 import cors from "cors";
+import { db } from "./db";
 
 // Add your routes here
-const setupApp = (client: Client): express.Application => {
+const setupApp = (): express.Application => {
   const app: express.Application = express();
 
   app.use(cors());
@@ -15,36 +16,15 @@ const setupApp = (client: Client): express.Application => {
   app.use(express.json());
 
   app.get("/examples", async (_req, res) => {
-    const { rows } = await client.query(`SELECT * FROM example_table`);
-    res.json(rows);
+    // const { rows } = await client.query(`SELECT * FROM example_table`);
+    res.json([]);
   });
 
   return app;
 };
 
-// Waits for the database to start and connects
-const connect = async (): Promise<Client> => {
-  console.log("Connecting");
-  const resource = `tcp:${process.env.PGHOST}:${process.env.PGPORT}`;
-  console.log(`Waiting for ${resource}`);
-  await waitOn({ resources: [resource] });
-  console.log("Initializing client");
-  const client = new Client();
-  await client.connect();
-  console.log("Connected to database");
-
-  // Ensure the client disconnects on exit
-  onExit(async () => {
-    console.log("onExit: closing client");
-    await client.end();
-  });
-
-  return client;
-};
-
 const main = async () => {
-  const client = await connect();
-  const app = setupApp(client);
+  const app = setupApp();
   const port = parseInt(process.env.SERVER_PORT!);
   app.listen(port, () => {
     console.log(`Draftbit Coding Challenge is running at http://localhost:${port}/`);
